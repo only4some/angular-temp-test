352e8e0650b2faa0a4cbd7147e56d5c2c5b8aae7
only4some <only4some@gmail.com>
2021-08-08 12:49:53 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-bcljss
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-bcljss)
---
+++ src/app/addhoc.component.ts
@@ -0,0 +1,6 @@
+import{Component} from '@angular/core';
+@Component({
+selector:'addhoc',
+template:'<ng-content></ng-content>'
+})
+export class AddhocComponent{}
