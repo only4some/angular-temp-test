30625176383d9d74a1eb1717390c1e2bce3d6b32
only4some <only4some@gmail.com>
2021-08-08 17:29:55 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import{AddhocComponent} from './addhoc.component';
+import {MessageComponent} from './message.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent,AddhocComponent  ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  entryComponents:[MessageComponent]
 })
 export class AppModule { }
